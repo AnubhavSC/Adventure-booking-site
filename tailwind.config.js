f5044b1de9b0f43ff3e464d3adff5ec37db51c72
AnubhavSc <anubhavnath65@gmail.com>
2023-10-01 00:34:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        kanit: 'Kanit',
+        Montserrat: 'Montserrat',
+        Archivo: 'Archivo Black'
+      },
+    },
+  },
+  plugins: [],
+}
+
